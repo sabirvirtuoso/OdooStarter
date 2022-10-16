e48f677bec93d1054f7b74d2499cdac95da03077
syedmusawi <syed.musawi@dsinnovators.com>
2022-10-16 07:09:44 +0600

    Initial Commit

---
+++ addons/my_widget/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    'name': 'Contact Widgets Module',
+    'version': '1.0.0',
+    'summary': 'Test Module',
+    'depends': ['base'],
+    'data': [],
+    'installable': True,
+    'auto_install': False,
+}
